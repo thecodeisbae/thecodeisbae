19a97f8b049127d0212611fc39146341b7237dc8
thecodeisbae <thecodeisbae@gmail.com>
2021-11-12 18:09:16 +0100

    adding_cmd

--- routes/router.php
+++ routes/router.php
@@ -4,11 +4,12 @@ include_once(_VENDOR_PATH.'\thecodeisbae\Routing\Route.php');
 use thecodeisbae\Routing\Route as Route;
 
 /** Retrieve necessary infos from the request **/
-$_full_url = explode('/',$_SERVER['REQUEST_URI']); 
-$_uri = $_full_url[1];
-$_url = $_SERVER['HTTP_HOST'].$_uri;
-$_method = $_SERVER['REQUEST_METHOD'];
-$_params = array_slice($_full_url,2);
+    $_full_url = explode('/',$_SERVER['REQUEST_URI']); 
+    $_uri = $_full_url[1];
+    $_url = $_SERVER['HTTP_HOST'].$_uri;
+    $_method = $_SERVER['REQUEST_METHOD'];
+    $_params = array_slice($_full_url,2);
+    
 
 /** Handle the uri using the right controller and function **/
 switch($_uri)
--- system/functions.php
+++ system/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-    function debug($args){
+    function debug($args)
+    {
         echo '<pre style="background-color:black;color:white;padding:25px;">Debug information<br><br>', print_r($args, 1),'</pre>';
         exit;
     }
@@ -14,4 +15,14 @@
     {
         echo '<pre style="background-color:black;color:white;padding:25px;">Running history<br><br>',print_r(debug_print_backtrace(),1),'</pre>';
         exit;
+    }
+
+    function assets($path)
+    {
+        return _ASSETS_PATH.$path;
+    }
+
+    function storage($path)
+    {
+        return _STORAGE_PATH.'files/'.$path;
     }
